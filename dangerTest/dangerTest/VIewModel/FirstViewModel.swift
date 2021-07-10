1f61ed19a9bd32ab4ca76059bfd2ccfabf225032
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-07-10 22:41:21 +0900

    기본 세팅중
     - gemfile, podfile 생성

---
+++ dangerTest/dangerTest/VIewModel/FirstViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FirstVIewModel.swift
+//  dangerTest
+//
+//  Created by 이장훈 on 2021/07/10.
+//
+
+import Foundation
+
+struct FirstViewModel {
+    
+    
+    
+    var testCaseOne: Bool {
+        false
+    }
+}
---
+++ dangerTest/dangerTestTests/FirstViewModelTest.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewModelTest.swift
+//  dangerTestTests
+//
+//  Created by 이장훈 on 2021/07/10.
+//
+
+import XCTest
+@testable import dangerTest
+
+class FirstViewModelTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+
+        let sut = FirstViewModel()
+        XCTAssertFalse(sut.testCaseOne)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
