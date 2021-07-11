b2b4b122435bd08e9328046509e2415ddce440b5
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-07-11 10:45:22 +0900

    16

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,7 +2,13 @@ import Danger
 import Foundation
 //import DangerSwiftHammer // package: https://github.com/el-hoshino/DangerSwiftHammer.git
 import DangerSwiftCoverage // package: https://github.com/f-meloni/danger-swift-coverage.git
+import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
+
 
 let danger = Danger()
+
+
+XCodeSummary(filePath: "result.json").report()
+
 
 Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 90)
