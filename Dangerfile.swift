a6e21f00976c6f9774bccf31e5824a85365a7181
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-07-16 23:23:22 +0900

    19

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -10,18 +10,7 @@ let danger = Danger()
 
 markdown("## New danger.github.pr.head : " + github.pullRequest.base.ref)
 
-slather.configure("DangerTest.xcodeproj", "DangerTest", options: {
-  workspace: 'DangerTest',
-  build_directory: 'DangerTest',
-  ci_service: github})
-
-slather.notify_if_coverage_is_less_than(minimum_coverage: 80)
-slather.notify_if_modified_file_is_less_than(minimum_coverage: 60)
-slather.show_coverage
-
 Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 90)
-
-
 
 
 SwiftLint.lint(.modifiedAndCreatedFiles(directory: nil), inline: true, configFile: ".swiftlint.yml")
