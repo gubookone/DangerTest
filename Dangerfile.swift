609efbb27a5df0861e4605d11e5af4b0e13e1a38
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-07-16 22:06:34 +0900

    17

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,9 +4,20 @@ import DangerSwiftCoverage // package: https://github.com/f-meloni/danger-swift-
 import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
 
 
+
+
 let danger = Danger()
 
-markdown("## New danger.github.pr.head" + danger.github.pullRequest.base.ref)
+markdown("## New danger.github.pr.head : " + github.pullRequest.base.ref)
+
+slather.configure("DangerTest.xcodeproj", "DangerTest", options: {
+  workspace: 'DangerTest',
+  build_directory: 'DangerTest',
+  ci_service: github})
+
+slather.notify_if_coverage_is_less_than(minimum_coverage: 80)
+slather.notify_if_modified_file_is_less_than(minimum_coverage: 60)
+slather.show_coverage
 
 Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 90)
 
