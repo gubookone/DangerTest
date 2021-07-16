df0fc02eed21148ac2d1bea7d0cd26f55184f249
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-07-16 11:26:59 +0900

    PR TEST 4

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,12 +1,10 @@
 import Danger
 import Foundation
-import DangerSwiftHammer // package: https://github.com/el-hoshino/DangerSwiftHammer.git
 import DangerSwiftCoverage // package: https://github.com/f-meloni/danger-swift-coverage.git
 import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
 
 
 let danger = Danger()
-
 
 XCodeSummary(filePath: "result.json").report()
 
