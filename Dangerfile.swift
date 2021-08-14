f191fd264a0608f31bbf361586da2af10beca23e
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-08-14 21:19:30 +0900

    test 131

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -11,7 +11,7 @@ let maxWarningsCount = 300
 let danger = Danger()
 let executor = ShellExecutor()
 
-Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 90)
+//Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 90)
 
 
 
@@ -21,20 +21,20 @@ Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 90)
 //let swiftC = try executor.execute("curl -H 'Accept: application/vnd.github.v3+json' 'https://api.github.com/repos/gubookone/DangerTest/'", arguments: [])
 //print(swiftC)
 
+//
+//let env = try executor.execute("env", arguments: [])
+//print(env)
+//
+//let number = "curl -SSL -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN -X POST -H 'Content-Type: application/json' -d '{\"labels\":[\"bug\"]}' https://api.github.com/repos/gubookone/DangerTest/issues/$PR_NUMBER/labels"
 
-let env = try executor.execute("env", arguments: [])
-print(env)
 
-let number = "curl -SSL -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN -X POST -H 'Content-Type: application/json' -d '{\"labels\":[\"bug\"]}' https://api.github.com/repos/gubookone/DangerTest/issues/$PR_NUMBER/labels"
-
-
-let env2 = try executor.execute("curl -SSL -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN -X POST -H 'Content-Type: application/json' -d '{\"labels\":[\"bug\"]}' https://api.github.com/repos/gubookone/DangerTest/issues/$PR_NUMBER/labels" , arguments: [])
+let env2 = try executor.execute("curl -X POST -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN https://api.github.com/repos/gubookone/DangerTest/issues/$PR_NUMBER/labels" , arguments: [])
 print(env2)
 
-let summary = XCodeSummary(filePath: "./build/reports/errors.json")
-
-if summary.warningsCount > maxWarningsCount {
-  fail("There are more than \(maxWarningsCount) warnings")
-}
-
-summary.report()
+//let summary = XCodeSummary(filePath: "./build/reports/errors.json")
+//
+//if summary.warningsCount > maxWarningsCount {
+//  fail("There are more than \(maxWarningsCount) warnings")
+//}
+//
+//summary.report()
