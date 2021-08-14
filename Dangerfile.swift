7b05920bd6371eb3495892deac2ae740102e9d07
janghunlee <janghun.lee@deliveryhero.co.kr>
2021-08-15 08:09:19 +0900

    TEST 101

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -28,13 +28,13 @@ let executor = ShellExecutor()
 //let number = "curl -SSL -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN -X POST -H 'Content-Type: application/json' -d '{\"labels\":[\"bug\"]}' https://api.github.com/repos/gubookone/DangerTest/issues/$PR_NUMBER/labels"
 
 //
-let env2 = try executor.execute("curl -X POST -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN https://api.github.com/repos/gubookone/DangerTest/pulls/$PR_NUMBER" , arguments: [])
-print(env2)
-//
+//let env2 = try executor.execute("curl -X POST -H Accept: application/vnd.github.v3+json -H Authorization: token $GITHUB_TOKEN https://api.github.com/repos/gubookone/DangerTest/pulls/$PR_NUMBER" , arguments: [])
+//print(env2)
+////
 
-let echo = "echo 'This action will add the label to the PR $PR_NUMBER'"
-let _ = try executor.execute(echo, arguments: [])
 
+let echo = try executor.execute("echo 'This action will add the label to the PR ${PR_NUMBER}'", arguments: [])
+print(echo)
 
 
 //let env2 = executor.execute("curl -H 'Accept: application/vnd.github.v3+json' -H 'Authorization: token $GITHUB_TOKEN' -X POST -d {'labels':'bug'} https://api.github.com/repos/gubookone/DangerTest/issues/$PR_NUMBER/labels" , arguments: [])
